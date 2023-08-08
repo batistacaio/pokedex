7b10f183f278687fd1885a378b09d941a4ff2a01
Caio Batista <caio_b_s@hotmail.com>
2023-08-08 15:35:58 -0300

    feat: add images and pokeapi.js adjusts

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,17 @@
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">#${pokemon.order}</span>
         <span class="name">${pokemon.name}</span>
         <div class="detail">
             <ol class="types">
-                <li class="type">Grass</li>
-                <li class="type">Poison</li>
+                ${convertPokemonTypesToLi(pokemon.types).join("")}
             </ol>
-            <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/1.svg" 
+            <img src="${pokemon.sprites.other.dream_world.front_default}" 
             alt="${pokemon.name}">
         </div>
     </li>
--- assets/pokeapi.js
+++ assets/pokeapi.js
@@ -1,9 +1,17 @@
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((res) => res.json())
         .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
         .catch((err) => console.log(err))
 }

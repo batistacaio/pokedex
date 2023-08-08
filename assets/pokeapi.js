8726008145d5ef202a311f1a8bf2070bbe171c02
Caio Batista <caio_b_s@hotmail.com>
2023-08-08 15:52:56 -0300

    fix: 'type is not a function'

--- assets/pokeapi.js
+++ assets/pokeapi.js
@@ -4,8 +4,10 @@ function convertPokeApiToPokemon(pokeDetail) {
     const pokemon = new Pokemon
     pokemon.name = pokeDetail.name
     pokemon.number = pokeDetail.order
-    pokemon.types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    pokemon.type = pokemon.types.get(0)
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const { type } = types
+    pokemon.types = types
+    pokemon.type = type
     pokemon.image = pokeDetail.sprites.other.dream_world.front_default
 
     return pokemon
@@ -14,9 +16,7 @@ function convertPokeApiToPokemon(pokeDetail) {
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
-        .then((pokemon) => {
-
-        })
+        .then(convertPokeApiToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {

c510cda218ef175e6dbaea2a2adc768f97a9bd10
Caio Batista <caio_b_s@hotmail.com>
2023-08-08 15:43:38 -0300

    feat: created pokemon model

--- assets/pokeapi.js
+++ assets/pokeapi.js
@@ -1,8 +1,22 @@
 const pokeApi = {}
 
+function convertPokeApiToPokemon(pokeDetail) {
+    const pokemon = new Pokemon
+    pokemon.name = pokeDetail.name
+    pokemon.number = pokeDetail.order
+    pokemon.types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    pokemon.type = pokemon.types.get(0)
+    pokemon.image = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
+        .then((pokemon) => {
+
+        })
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
